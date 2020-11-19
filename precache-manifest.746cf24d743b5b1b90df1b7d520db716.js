6d8444ab5f4d5de53de277fd094e1bd2c865b3f4
GatoVampiro <msebass1@gmail.com>
2020-11-19 11:28:17 -0500

    Updates

---
+++ precache-manifest.746cf24d743b5b1b90df1b7d520db716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a2b4c25c7e7807ed5f99695952ebed",
+    "url": "/Vimpare-Calendar/index.html"
+  },
+  {
+    "revision": "e767e69a13ae9bd1a71f",
+    "url": "/Vimpare-Calendar/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "35183ac0e12c506ff331",
+    "url": "/Vimpare-Calendar/static/js/2.d97ce545.chunk.js"
+  },
+  {
+    "revision": "19cd89fbf3150df35a3d403decf45313",
+    "url": "/Vimpare-Calendar/static/js/2.d97ce545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e767e69a13ae9bd1a71f",
+    "url": "/Vimpare-Calendar/static/js/main.6427076f.chunk.js"
+  },
+  {
+    "revision": "aa01a1b1b6996fa5e55a",
+    "url": "/Vimpare-Calendar/static/js/runtime-main.8fc63d38.js"
+  }
+]);

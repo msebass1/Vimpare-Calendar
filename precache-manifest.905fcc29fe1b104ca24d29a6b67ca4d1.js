86e64fd1f29dd5afdbdcd30675a50aeefd1d598a
GatoVampiro <msebass1@gmail.com>
2020-12-04 16:31:22 -0500

    Updates

---
+++ precache-manifest.905fcc29fe1b104ca24d29a6b67ca4d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30cead12eedd366ac834f914485d6bec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35c8fcae9a697410a413",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "309914ba65eb69c6f93d",
+    "url": "/static/js/2.c0f32c06.chunk.js"
+  },
+  {
+    "revision": "19cd89fbf3150df35a3d403decf45313",
+    "url": "/static/js/2.c0f32c06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c8fcae9a697410a413",
+    "url": "/static/js/main.410cea54.chunk.js"
+  },
+  {
+    "revision": "7913bdb217681d6e1e07",
+    "url": "/static/js/runtime-main.97f4d32f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Vimpare-Calendar/precache-manifest.03b8add27a32c3f2f9f5433d91adc552.js"
+  "/precache-manifest.905fcc29fe1b104ca24d29a6b67ca4d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Vimpare-Calendar/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
